398a65f106441fef3202003a065d4bb92f875af0
chevyshevy97 <supadhayay3@gatech.edu>
2018-02-18 20:14:51 -0500

    Modifying python functions for graphs page

---
+++ monitor/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'dorms'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^tips/$', views.tips, name='tips'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^comparison/$', views.about, name='about'),
+    url(r'^graph/$', views.about, name='about'),
+    url(r'^dorms/$', views.DormListView.as_view(), name='list'),
+    url(r'^dorms/(?P<pk>[0-9]+)/$', views.DormDetailView, name='detail'),
+    url(r'^dorms/(?P<pk>[0-9]+)/addenergyreading$', views.add_reading),
+]
---
+++ monitor/views.py
@@ -0,0 +1,67 @@
+from django.http      import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404, render
+from django.views     import generic
+from .models          import Dorm, Tip, About, EnergyReading, Comparison, Graph
+from ContactForm     import ContactForm
+import datetime
+
+def index(request):
+    all_dorms = Dorm.objects.all()
+    context = {
+        'dorm_list': all_dorms,
+    }
+    return render(request, 'dorm/index.html', context)
+
+def tips(request):
+    model = Tip
+    template_name = 'dorm/tips.html'
+    return render(request, 'dorm/tips.html')
+
+def about(request):
+    model = About
+    template_name = 'dorm/about.html'
+    if request.method == 'POST':
+        print ContactForm
+        form = ContactForm(request.POST)
+        print form.data['content']
+        message = "Your submission was received. Thank you!"
+        return render(request, 'dorm/about.html', {'data': form.data['content'], 'message': message})
+    else:
+        contact_form = ContactForm()
+        message = "Leave us feedback and/or questions!"
+        return render(request, 'dorm/about.html', {
+            'form': contact_form,'message': message, 'show': True
+        })
+
+
+def comparison(request):
+    model = Tip
+    template_name = 'dorm/tips.html'
+    return render(request, 'dorm/comparison.html')
+
+def graph(request):
+    model = Tip
+    template_name = 'dorm/tips.html'
+    return render(request, 'dorm/graph.html')
+
+class DormListView(generic.ListView):
+    model = Dorm
+    template_name = 'dorm/list.html'
+
+def DormDetailView(request):
+    model = Dorm
+    template_name = 'dorm/detail.html'
+    context = {
+        'dorm_list': all_dorms,
+    }
+    return render(request, 'dorm/detail.html')
+
+
+def add_reading(request, pk):
+    print(request.body)
+    if request.method == 'POST' and request.POST and 'power' in request.POST:
+        power = request.POST['power']
+        dorm = Dorm.objects.get(pk=int(pk))
+        dorm.energyreading_set.create(power=power)
+        return HttpResponse("Success")
+    return HttpResponseBadRequest()
