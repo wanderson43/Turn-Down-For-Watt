575042a6adb06f3edb21f7dbc1833b49c536da3a
Faisal <faisalgedi@gmail.com>
2018-02-18 19:38:27 -0500

    image work

--- energyjackets/settings.py
+++ energyjackets/settings.py
@@ -1,4 +1,4 @@
-import os
+import os.path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -92,6 +92,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join('../static'),
+    '../energyjackets/../static/',
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -109,4 +115,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
+STATIC_ROOT = ' '
